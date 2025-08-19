a7f20991feaa37830f665d3ddabff7f4977861b7
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-08-20 00:16:20 +0300

    add then for promise2, catch for promise1 and edit catch message of 2nd promise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // write your code here
-const promise1 = new Promise((resolve) => {
+const promise1 = new Promise((resolve, reject) => {
   const logo = document.querySelector('.logo');
 
   return logo.addEventListener('click', () => resolve());
@@ -11,11 +11,15 @@ const promise2 = new Promise((resolve, reject) => {
   setTimeout(() => reject(new Error('Promise rejected after 3s')), 3000);
 });
 
-promise1.then(() => showMessage('Promise was resolved!', 'message'));
+promise1
+  .then(() => showMessage('Promise was resolved!', 'message'))
+  .catch(() => 'Promise was rejected!');
 
-promise2.catch(() => {
-  showMessage('Promise was resolved!', 'message error-message');
-});
+promise2
+  .then(() => 'Promise was resolved!')
+  .catch(() => {
+    showMessage('Promise was rejected!', 'message error-message');
+  });
 
 function showMessage(text, className) {
   const div = document.createElement('div');
